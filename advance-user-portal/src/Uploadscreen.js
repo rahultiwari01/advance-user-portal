b3893eecab4b65135efc5e7f38ad55a784e2c2dc
Rahul Tiwari <rahulti011@gmail.com>
2020-05-25 19:22:25 +0530

    Working user module

---
+++ advance-user-portal/src/Uploadscreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Uploadscreen extends Component {
+    render(){
+        return(
+        <div> 
+            <h1> My Login successfull</h1>
+        </div>
+        )
+    }
+    }
+
+export default Uploadscreen;
